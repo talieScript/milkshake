9bca5cb99078a6e83f931b7e55c8bf333ee1df7a
Taliesin <taliesin.bowes@hotmail.co.uk>
2021-02-21 22:33:13 +0000

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
